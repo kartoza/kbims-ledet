f50b7963107ffbc4dc1c275eb20a714320ca3b40
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2018-07-11 15:21:20 +0700

    Include production docker build script

---
+++ deployment/production/uwsgi/build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+if [ -z "$REPO_NAME" ]; then
+	REPO_NAME=kartoza
+fi
+
+if [ -z "$IMAGE_NAME" ]; then
+	IMAGE_NAME=ledet_uwsgi
+fi
+
+if [ -z "$TAG_NAME" ]; then
+	TAG_NAME=latest
+fi
+
+if [ -z "$BUILD_ARGS" ]; then
+	BUILD_ARGS="--pull --no-cache"
+fi
+
+# Build Args Environment
+
+if [ -z "$LEDET_TAG" ]; then
+	LEDET_TAG=develop
+fi
+
+echo "LEDET_TAG=${LEDET_TAG}"
+
+echo "Build: $REPO_NAME/$IMAGE_NAME:$TAG_NAME"
+
+docker build -t ${REPO_NAME}/${IMAGE_NAME} \
+	--build-arg LEDET_TAG=${LEDET_TAG} \
+	${BUILD_ARGS} .
+docker tag ${REPO_NAME}/${IMAGE_NAME}:latest ${REPO_NAME}/${IMAGE_NAME}:${TAG_NAME}
+docker push ${REPO_NAME}/${IMAGE_NAME}:${TAG_NAME}
