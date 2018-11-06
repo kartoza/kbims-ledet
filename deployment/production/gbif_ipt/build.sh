b421930b606a305c077a109695c8446eeaefafb8
Dimas <dimas.ciputra@gmail.com>
2018-11-06 14:40:40 +0700

    Add gbif ipt docker configuration

---
+++ deployment/production/gbif_ipt/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if [ -z "$REPO_NAME" ]; then
+	REPO_NAME=kartoza
+fi
+
+if [ -z "$IMAGE_NAME" ]; then
+	IMAGE_NAME=gbif_ipt
+fi
+
+if [ -z "$TAG_NAME" ]; then
+	TAG_NAME=latest
+fi
+
+if [ -z "$BUILD_ARGS" ]; then
+	BUILD_ARGS="--pull --no-cache"
+fi
+
+# Build Args Environment
+
+echo "Build: $REPO_NAME/$IMAGE_NAME:$TAG_NAME"
+
+docker build -t ${REPO_NAME}/${IMAGE_NAME} \
+	${BUILD_ARGS} .
+docker tag ${REPO_NAME}/${IMAGE_NAME}:latest ${REPO_NAME}/${IMAGE_NAME}:${TAG_NAME}
+docker push ${REPO_NAME}/${IMAGE_NAME}:${TAG_NAME}
